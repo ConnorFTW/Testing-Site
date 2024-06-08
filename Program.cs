f5b11e53dedb50ad0e8b1e7163bc50c14a7028a1
ConnorFTW <72752531+ConnorFTW@users.noreply.github.com>
2024-06-08 16:43:47 -0500

    Test

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿// MyWebApp/Program.cs
+using System;
+using System.Net;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace MyWebApp
 {
@@ -6,7 +12,29 @@ namespace MyWebApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello from C#!");
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .ConfigureServices(services => services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>())
+                .Configure(app =>
+                {
+                    app.Run(context =>
+                    {
+                        if (context.Request.Path == "/api/doSomething")
+                        {
+                            // Your basic C# function logic here
+                            Console.WriteLine("C# Function Executed!");
+                            return context.Response.WriteAsync("C# Function Executed!");
+                        }
+                        else
+                        {
+                            context.Response.StatusCode = (int)HttpStatusCode.NotFound;
+                            return context.Response.WriteAsync("Page not found");
+                        }
+                    });
+                })
+                .Build();
+
+            host.Run();
         }
     }
 }
