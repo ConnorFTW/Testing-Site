e32d754e272d8c4289dff4e13bcf3f9958b2a303
ConnorFTW <72752531+ConnorFTW@users.noreply.github.com>
2024-06-08 15:30:24 -0500

    Update CSharpFunction.aspx.cs

--- CSharpFunction.aspx.cs
+++ CSharpFunction.aspx.cs
@@ -4,7 +4,6 @@ using System.Web.Services;
 public partial class CSharpFunction : System.Web.UI.Page
 {
     [WebMethod]
-    [System.Web.Script.Services.ScriptMethod(UseHttpGet = false)] // Ensures that POST requests are accepted
     public static string GetMessage()
     {
         return "Hello from C# function!";
